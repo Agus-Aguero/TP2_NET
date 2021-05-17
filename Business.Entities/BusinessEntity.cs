cd65180004a226e50b252db5dc6e00c35e5649ad
Agus-Aguero <81131849+Agus-Aguero@users.noreply.github.com>
2021-05-17 20:10:56 -0300

    Firts Commit
    Mi primer programa!

---
+++ Business.Entities/BusinessEntity.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    public class BusinessEntity
+    {
+        /* Esta clase contrendra los elementos basicos que compartiran las entidades
+         de nuestro sistema y luego heredaran de ella */
+
+        private int _ID;
+        public int ID
+        {
+            get { return _ID; }
+            set {_ID = value;} 
+        }
+
+        private States _State;
+        public States State 
+        { 
+            get { return _State; }
+            set {_State= value; }
+        }
+
+        public BusinessEntity()
+        {
+            this.State = States.New;
+        }
+
+        public enum States
+        {
+            New=1, 
+            Deleted=2, 
+            Modified=3, 
+            Unmodified=4
+        }
+
+
+    }
+}
---
+++ Business.Entities/ModuloUsuario.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Entities
+{
+    class ModuloUsuario
+    {
+        public int IdUsuario { get; set; }
+        public int IdModulo { get; set; }
+        public bool PermiteAlta { get; set; }
+        public bool PermiteBaja { get; set; }
+        public bool PermiteModificacion { get; set; }
+        public bool PermiteConsulta { get; set; }
+
+
+
+    }
+}
