76c496ada22c52e080c0e8d0f61513f081a4186c
Agus-Aguero <81131849+Agus-Aguero@users.noreply.github.com>
2021-05-19 23:05:11 -0300

    Avances ejercicio 3

---
+++ Business.Logic/UsuarioLogic.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Business.Entities;
+using Data.Database;
+
+namespace Business.Logic
+{
+    public class UsuarioLogic : BusinessLogic
+    {
+        public UsuarioLogic()
+            {}
+
+        
+        public Data.Database.UsuarioAdapter UsuarioData { get; set; }
+
+
+        #region Metodos
+        /*
+        public Usuario GetOne(int ID)
+        { 
+
+        }*/
+
+        public List<Usuario> GetAll()
+        {
+                                    // REVISAR SI ESTO ESTA BIEN 
+
+            List<Usuario> usuarios = new List<Usuario>();
+            return usuarios;
+        }
+
+        public void Save()
+        { }
+
+        public void Delete()
+        { }
+
+        #endregion
+
+    }
+}
