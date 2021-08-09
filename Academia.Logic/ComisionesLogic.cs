87581eba39dbc8816195a55f65db453825d9e949
Agus-Aguero <81131849+Agus-Aguero@users.noreply.github.com>
2021-08-08 23:55:09 -0300

    Avance Tp2

---
+++ Academia.Logic/ComisionesLogic.cs
@@ -0,0 +1,13 @@
+﻿using Academia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Academia.Logic
+{
+    public class ComisionesLogic : BusinessLogic<comisiones>
+    {
+    }
+}
