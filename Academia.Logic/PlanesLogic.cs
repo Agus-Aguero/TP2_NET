d640253d958f64064b1cbf69731a00bf6de32786
Agus-Aguero <81131849+Agus-Aguero@users.noreply.github.com>
2021-08-09 19:18:07 -0300

    Cambios

--- Academia.Logic/PlanesLogic.cs
+++ Academia.Logic/PlanesLogic.cs
@@ -1,13 +1,13 @@
-﻿using System;
+﻿using Academia.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Academia.Entities;
 
 namespace Academia.Logic
 {
-    public class PlanesLogic: BusinessLogic<planes>
+    public class PlanesLogic:BusinessLogic<planes>
     {
     }
 }
