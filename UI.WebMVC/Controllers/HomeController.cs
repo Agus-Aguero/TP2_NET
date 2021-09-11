15c6df1e4da8b04278e744a7ade6f9833a25eb62
leoksros <leonardopmiceli@gmail.com>
2021-09-11 18:34:51 -0300

    login con mvc a medias

---
+++ UI.WebMVC/Controllers/HomeController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Academia.Entities;
+using Academia.Logic;
+using Academia.EntityFramework;
+
+
+namespace UI.WebMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(usuarios userDataFromPost)
+        {
+
+            UsuarioRepository usuarioRepository = new UsuarioRepository();
+            usuarios usuario = usuarioRepository.findByUserName(userDataFromPost.nombre_usuario);
+
+            if (usuario != null && usuario.clave == userDataFromPost.clave)
+            {
+                return View(usuario);
+            }
+            else
+            {
+                return View("Index");
+            }
+        }
+    }
+}
