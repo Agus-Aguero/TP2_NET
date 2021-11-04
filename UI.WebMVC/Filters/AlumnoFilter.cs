29528f23adc861fedea4e0ff5eb2c11e4a2cfa3d
Leandro Montenegro <48534595+leanm0903@users.noreply.github.com>
2021-11-03 23:20:02 -0300

    Se agregan filters

---
+++ UI.WebMVC/Filters/AlumnoFilter.cs
@@ -0,0 +1,31 @@
+﻿using Academia.Entities;
+using Academia.Util;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using UI.WebMVC.Controllers;
+
+namespace UI.WebMVC.Filters
+{
+    public class AlumnoFilter: ActionFilterAttribute
+    {
+        private usuarios usuario;
+        public override void OnActionExecuted(ActionExecutedContext filterContext)
+        {
+            base.OnActionExecuted(filterContext);
+            usuario = (usuarios)HttpContext.Current.Session["User"];
+            if (usuario.personas.tipo_persona == TipoPersona.Alumno)
+            {
+
+                if ((filterContext.Controller is HomeController) == false)
+                {
+
+                    filterContext.HttpContext.Response.Redirect("~/Home");
+
+                }
+            }
+        }
+    }
+}
--- UI.WebMVC/Filters/VerificaSession.cs
+++ UI.WebMVC/Filters/VerificaSession.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 using UI.WebMVC.Controllers;
 
 namespace UI.WebMVC.Filters
@@ -11,18 +12,25 @@ namespace UI.WebMVC.Filters
     public class VerificaSession:ActionFilterAttribute
     {
         private usuarios usuario;
-        public override void OnActionExecuted(ActionExecutedContext filterContext)
+      
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
         {
-            base.OnActionExecuted(filterContext);
             usuario = (usuarios)HttpContext.Current.Session["User"];
             if (usuario == null)
             {
-                
-                if((filterContext.Controller is HomeController) ==false )
+
+                if ((filterContext.Controller is HomeController) == false)
                 {
-                    filterContext.HttpContext.Response.Redirect("~/Home");
+
+                    RouteValueDictionary redirectTargetDictionary = new RouteValueDictionary();
+                    redirectTargetDictionary.Add("controller", "Home");
+
+                    filterContext.Result = new RedirectToRouteResult(redirectTargetDictionary);
+
+
 
                 }
+                base.OnActionExecuting(filterContext);
             }
         }
     }
